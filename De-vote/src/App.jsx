c146c2c08fbe17b025dd29cab73d23a401dce0bb
Shijin A <shijinasasi835@gmail.com>
2024-04-06 13:19:44 +0530

    first commit

--- De-vote/src/App.jsx
+++ De-vote/src/App.jsx
@@ -8,7 +8,7 @@ import Voter from './pages/Voter.jsx';
 import Elections from './pages/Elections.jsx';
 import Results from './pages/Results.jsx';
 import Login from './pages/Login/Login.jsx';
-import RegisterForm from './pages/Register/RegisterForm.jsx';
+import POLogin from './pages/Register/POLogin.jsx'
 import AdminLogin from './pages/AdminLogin/AdminLogin.jsx';
 import { Routes, Route, BrowserRouter as Router, useLocation, Navigate } from 'react-router-dom';
 
@@ -20,12 +20,12 @@ function AppWithHeader() {
   const {user}=useAuthContext()
 
   // Paths where the Header should not be displayed
-  const noHeaderPaths = ['/', '/signup', '/admin'];
+  const noHeaderPaths = ['/', '/pologin', '/admin'];
 
   // Check if the current path is one of the paths where Header should not be displayed
   const showHeader = !noHeaderPaths.includes(location.pathname);
 
-  const isLoginOrRegister = location.pathname === '/' || location.pathname === '/signup' || location.pathname === '/admin';
+  const isLoginOrRegister = location.pathname === '/' || location.pathname === '/pologin' || location.pathname === '/admin';
   const pageClass = isLoginOrRegister ? 'custom-background' : 'normal-background';
 
 
@@ -45,7 +45,7 @@ function AppWithHeader() {
           <Route path="/elections" element={user?<Elections />:<Navigate to='/'/>} />
           <Route path="/results" element={user?<Results />:<Navigate to='/'/>} />
           <Route index={true} element={<Login/>}/>
-          <Route path="/signup" element={<RegisterForm />} />
+          <Route path="/pologin" element={<POLogin />} />
           <Route path="/admin" element={<AdminLogin />} />
           {/* Add more routes as needed */}
         </Route>
--- De-vote/src/pages/Register/POLogin.jsx
+++ De-vote/src/pages/Register/POLogin.jsx
@@ -1,10 +1,10 @@
 import React, { useState } from 'react';
 import { useNavigate } from 'react-router-dom';
-import './RegisterForm.css';
+import './POLogin.css';
 import NavBar from '../../components/NavBar/NavBar';
 import { Link } from 'react-router-dom';
 
-function RegisterForm() { 
+function POLogin() { 
   const [email, setEmail] = useState('');
   const [ktuid, setKtuid] = useState('');
   const [password, setPassword] = useState('');
@@ -41,7 +41,7 @@ function RegisterForm() {
         <NavBar />
         <div className="register">
             <div className='login'>
-                <div className="title"><h3 className='txt'>Voter Register</h3></div>
+                <div className="title"><h3 className='txt'>Presiding Officer</h3></div>
                 <div className="form">
                     <form onSubmit={handleSubmit}>
                         <input
@@ -52,14 +52,7 @@ function RegisterForm() {
                             value={email}
                             onChange={(e) => setEmail(e.target.value)}
                         />
-                        <input
-                            className='h-9 w-3/4 rounded-lg ml-10 mt-4'
-                            type="text"
-                            name='ktuid'
-                            placeholder='KTU ID'
-                            value={ktuid}
-                            onChange={(e) => setKtuid(e.target.value)}
-                        />
+        
                         <input
                             className='h-9 w-3/4 rounded-lg ml-10 mt-[15px]'
                             type='password'
@@ -69,17 +62,15 @@ function RegisterForm() {
                             onChange={(e) => setPassword(e.target.value)}
                         />
                         <div className='pt-4 pr-11 flex justify-end'>
-                            <input className='h-7 w-1/4 bg-purple-800 rounded-lg text-white cursor-pointer mr-[16px]' type='submit' value='Register'/>
+                            <input className='h-7 w-1/4 bg-purple-800 rounded-lg text-white cursor-pointer mr-[16px]' type='submit' value='Login'/>
                         </div>
                     </form>
                 </div> 
-                <div className='description'>
-                    <p>Already have an account? <Link className='text-blue-800' to="/">Login</Link></p>
-                </div>       
+                       
             </div>
         </div>
     </div>
   );
 }
 
-export default RegisterForm;
+export default POLogin;
